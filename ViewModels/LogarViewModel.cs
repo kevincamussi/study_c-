cdf6bb23af36b20b63064efb2449827879f80b8c
kevincamussi <kevincamussi@gmail.com>
2024-09-25 00:47:22 -0300

    login test commit using C#

---
+++ ViewModels/LogarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfLogin.Commands;
+
+namespace WpfLogin.ViewModels
+{
+    public class LogarViewModel
+    {
+        public LogarCommand Logar{ get; set; }
+
+        public LogarViewModel()
+        {
+            Logar = new LogarCommand();
+        }
+    }
+}
